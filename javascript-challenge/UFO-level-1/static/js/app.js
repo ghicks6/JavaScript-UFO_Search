2181b677c7af2aa76ad74dae101843b9f6abd6a7
Grant Hicks <70181086+ghicks6@users.noreply.github.com>
2020-12-20 00:12:45 -0500

    Finished UFO-level-1 JS script

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -1,32 +1,51 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// Select the button
-var button = d3.select("#filter-btn");
+// Display all data in table to start
+function displayData(allData){ 
+  tbody.text("")
+  allData.forEach(function(ufoSighting){
+  newTr = tbody.append("tr")
+  Object.entries(ufoSighting).forEach(function([key, value]){
+      newTd = newTr.append("td").text(value)	
+  })
+})}
+
+console.log(tableData);
+displayData(tableData);
 
 // Select the form
-var form = d3.select("#form-group");
-
-// Create event handlers 
-button.on("click", runEnter);
+var form = d3.select("#form-control");
+// Select the button
+var button = d3.select("#filter-btn");
+// Start function on click or enter
+button.on("click", (runEnter));
 form.on("submit",runEnter);
-
-// Complete the event handler function for the form
+// Complete the event handler function for the filter
 function runEnter() {
-
   // Prevent the page from refreshing
   d3.event.preventDefault();
-
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var dateInput = d3.select("#datetime").property("value")
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  if (dateInput.trim() === "" ) {
+    // Display the whole database if the date field has no date
+    var filteredData = tableData;}
+    else {
+      // otherwise, display the filtered dataset
+      var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === dateInput.trim());
+    };
 
-  console.log(inputValue);
-  console.log(tableData);
-
-  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+      // Display message if no records found
+  if (filteredData.length == 0) {
+    d3.select("tbody")
+      .append("tr")
+      .append("td")
+        .attr("colspan", 7)
+        .html("<h4>No Records Found</h4>");
+  };
 
   console.log(filteredData);
+  displayData(filteredData);
 };
